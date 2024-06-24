139b938b8ba5c601a3bc025d51c6ef0b9a638aab
Abdul <abdulx9999@gmail.com>
2024-06-24 09:57:40 +0100

    First Commit

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 # Show app title and description.
 st.set_page_config(page_title="Support ticket workflow", page_icon="🎫")
-st.title("🎫 Support ticket workflowTest123")
+st.title("🎫 Support ticket workflow")
 st.write(
     """
     This app shows how you can build an internal tool in Streamlit. Here, we are 
